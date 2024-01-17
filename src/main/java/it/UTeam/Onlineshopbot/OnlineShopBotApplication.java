91739ff23769bb71510ff76f1aaa067e92e7c6c9
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2024-01-16 22:46:09 -0800

    v1

---
+++ src/main/java/it/UTeam/Onlineshopbot/OnlineShopBotApplication.java
@@ -0,0 +1,13 @@
+package it.UTeam.Onlineshopbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/UTeam/Onlineshopbot/OnlineShopBotApplicationTests.java
@@ -0,0 +1,13 @@
+package it.UTeam.Onlineshopbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
