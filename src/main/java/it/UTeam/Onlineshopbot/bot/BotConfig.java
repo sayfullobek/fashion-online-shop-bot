19516afa553144bcbd1f324809305d46b2c544a9
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2024-01-23 02:36:57 -0800

    error update new

--- src/main/java/it/UTeam/Onlineshopbot/bot/BotConfig.java
+++ src/main/java/it/UTeam/Onlineshopbot/bot/BotConfig.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public interface BotConfig {
-    String USERNAME = "onlien_fashion_bot";
-    String TOKEN = "6955516191:AAFW5yhFZ5_NslvmmoWFcVJL2gRIibtxWUs";
+    //    String USERNAME = "onlien_fashion_bot";
+    String USERNAME = "firstWebApp_bot";
+    //    String TOKEN = "6955516191:AAFW5yhFZ5_NslvmmoWFcVJL2gRIibtxWUs";
+    String TOKEN = "6480156096:AAHkSd4AqtyeJ7VC4k2qO3FkE2rHDq5_vyM";
     List<String> START_BTN = Arrays.asList(
             "Kiyimlarni ko'rish", "Biz bilan bo'glanish", "Sozlamalar"
     );
